da9a0f8087fcbd5612f2a1fd3ee76f158be2f300
Sadriicaa <santirodrigmoca@outlook.com>
2024-05-16 03:01:02 -0500

    comun fixed - 3

--- Code/flaskr/templates/Comun.php
+++ Code/flaskr/templates/Comun.php
@@ -10,23 +10,7 @@
             <li id="Tendencias"><a href="{{ url_for('web.tendencias')}}">Tendencias</a>
             </li>
             
-            <?php /*
-            session_start();
-            if (isset($_SESSION['user'])) {
-                $user = $_SESSION['user'];
-		echo "<li id='dashBoard'><a href='{{url_for('web.dashboard')}}'>Dashboard</a></li>";
-
-		echo "<li id='logout' style='float: right;margin-right: 5% ;  margin-top: 1.5%;' ><a href='{{url_for('web.logout')}}'>Cerrar sesión</a></li>";
-                echo "<li id='usuario' style='float: right;margin-top: 1.5%;'><a href=''>$user</a></li>";
-                
-            } else {
-               
-                echo "<li id='registrarse' style='float: right;margin-right: 5% ;  margin-top: 1.5%;' ><a href='{{url_for('web.register')}}'>Registrarse</a></li>";
-		echo "<li id='iniciarSesion' style='float: right;margin-top: 1.5%;'><a href='{{url_for('web.login')}}'>Iniciar Sesión</a></li>";
-                
-            } */
-	    ?>
-        </ul>
+	</ul>
     </nav>
     <br>
 </div>
